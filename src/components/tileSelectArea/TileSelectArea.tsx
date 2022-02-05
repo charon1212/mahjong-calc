76460278debec3895a52a5c608cbfdfcb78e6e6d
charon1212 <charons2turquoise@gmail.com>
2022-02-05 17:35:52 +0900

    add onClick prop

--- src/components/tileSelectArea/TileSelectArea.tsx
+++ src/components/tileSelectArea/TileSelectArea.tsx
@@ -1,16 +1,18 @@
 import { Tile as TileType } from '../../domain/type';
 import Tile from '../tile/Tile';
 
-type Props = {};
+type Props = {
+  onClick?: (tile?: TileType) => unknown;
+};
 const TileSelectArea = (props: Props) => {
-  const {} = props;
+  const { onClick } = props;
   return (
     <>
       <div style={{ width: '300px' }}>
-        <TileSelectRow s='m' />
-        <TileSelectRow s='p' />
-        <TileSelectRow s='s' />
-        <TileSelectRow s='j' />
+        <TileSelectRow s='m' onClick={onClick} />
+        <TileSelectRow s='p' onClick={onClick} />
+        <TileSelectRow s='s' onClick={onClick} />
+        <TileSelectRow s='j' onClick={onClick} />
       </div>
     </>
   );
@@ -18,16 +20,17 @@ const TileSelectArea = (props: Props) => {
 
 type PropsTileSelectRow = {
   s: TileType['s'];
+  onClick?: (tile?: TileType) => unknown;
 };
 const TileSelectRow = (props: PropsTileSelectRow) => {
-  const { s } = props;
+  const { s, onClick } = props;
   const max = s === 'j' ? 7 : 9;
   const arr = [...Array(9)].map((_, i) => i + 1);
   return (
     <>
       <div style={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'space-between' }}>
         {arr.map((i) => (
-          <Tile tile={i <= max ? { n: i, s } : undefined} divStyle={{ width: '11%' }} />
+          <Tile tile={i <= max ? { n: i, s } : undefined} divStyle={{ width: '11%' }} onClick={onClick} />
         ))}
       </div>
     </>
