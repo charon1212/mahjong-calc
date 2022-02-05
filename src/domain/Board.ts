9c6a5f1e53156a75f33ec54b2ef29aec6723dce7
charon1212 <charons2turquoise@gmail.com>
2022-02-05 17:36:08 +0900

    change Board method (class base -> function base)

--- src/domain/Board.ts
+++ src/domain/Board.ts
@@ -1,33 +1,25 @@
 import { BoardInfo, Tile, TileSort } from "./type";
 
+export const cloneBoard = (board: BoardInfo) => {
+  return JSON.parse(JSON.stringify(board)) as BoardInfo;
+};
+export const addTile = (board: BoardInfo, tile: Tile) => {
+  board[tile.s][tile.n - 1] += 1;
+  return board;
+};
+export const deleteTile = (board: BoardInfo, tile: Tile) => {
+  board[tile.s][tile.n - 1] -= 1;
+  return board;
+};
+export const getInitialBoard = (): BoardInfo => ({
+  m: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  p: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  s: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  j: [0, 0, 0, 0, 0, 0, 0],
+});
 const tileSortKeys: TileSort[] = ['m', 'p', 's', 'j',];
-export class Board {
-
-  public board: BoardInfo = {
-    m: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    p: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    s: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    j: [0, 0, 0, 0, 0, 0, 0],
-  };
-  constructor() { }
-  reset() {
-    this.board = {
-      m: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      p: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      s: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      j: [0, 0, 0, 0, 0, 0, 0],
-    };
-  }
-  add(tile: Tile) {
-    this.board[tile.s][tile.n - 1] += 1;
-  }
-  delete(tile: Tile) {
-    this.board[tile.s][tile.n - 1] -= 1;
-  }
-  getLength(): number {
-    return tileSortKeys
-      .map((key) => this.board[key].reduce((p, c) => p + c, 0))
-      .reduce((p, c) => p + c, 0);
-  }
-
-}
+export const getBoardLength = (board: BoardInfo) => {
+  return tileSortKeys
+    .map((key) => board[key].reduce((p, c) => p + c, 0))
+    .reduce((p, c) => p + c, 0);
+};
