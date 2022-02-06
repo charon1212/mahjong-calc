2dd47b854a1e629f0da3ae2a3f759b41f6338760
charon1212 <charons2turquoise@gmail.com>
2022-02-06 16:24:17 +0900

    refactoring

--- src/domain/mahjong/Board.ts
+++ src/domain/mahjong/Board.ts
@@ -1,14 +1,15 @@
+import { maxBoardLength, maxTileLength, tileSortKeys } from "./const";
 import { BoardInfo, Tile, TileSort } from "./type";
 
 export const cloneBoard = (board: BoardInfo) => {
   return JSON.parse(JSON.stringify(board)) as BoardInfo;
 };
 export const addTile = (board: BoardInfo, tile: Tile) => {
-  board[tile.s][tile.n - 1] += 1;
+  if (getBoardLength(board) < maxBoardLength) board[tile.s][tile.n - 1] = Math.min(maxTileLength, board[tile.s][tile.n - 1] + 1);
   return board;
 };
 export const deleteTile = (board: BoardInfo, tile: Tile) => {
-  board[tile.s][tile.n - 1] -= 1;
+  board[tile.s][tile.n - 1] = Math.max(0, board[tile.s][tile.n - 1] - 1);
   return board;
 };
 export const getInitialBoard = (): BoardInfo => ({
@@ -17,7 +18,6 @@ export const getInitialBoard = (): BoardInfo => ({
   s: [0, 0, 0, 0, 0, 0, 0, 0, 0],
   j: [0, 0, 0, 0, 0, 0, 0],
 });
-const tileSortKeys: TileSort[] = ['m', 'p', 's', 'j',];
 export const getBoardLength = (board: BoardInfo) => {
   return tileSortKeys
     .map((key) => board[key].reduce((p, c) => p + c, 0))
---
+++ src/domain/mahjong/const.ts
@@ -0,0 +1,5 @@
+import { TileSort } from "./type";
+
+export const maxBoardLength = 14;
+export const maxTileLength = 4;
+export const tileSortKeys: TileSort[] = ['m', 'p', 's', 'j',];
