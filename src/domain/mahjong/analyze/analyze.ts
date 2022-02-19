5528623566ae25705b3a7eacb63aa976f6ccef17
charon1212 <charons2turquoise@gmail.com>
2022-02-19 19:39:29 +0900

    refactoring

--- src/domain/mahjong/analyze/analyze.ts
+++ src/domain/mahjong/analyze/analyze.ts
@@ -9,7 +9,7 @@ export type AnalyzeResult =
   { rest: 1, expect: Rest1Expect } | // rest1は、テンパイ
   { rest: 2, expect: Rest2Expect } | // rest2は、1シャンテン
   { rest: 3, expect: Rest3Expect } | // rest3は、2シャンテン
-  undefined;
+  { rest: -1 };
 
 export const analyzeBoard = (b: BoardInfo): AnalyzeResult => {
 
@@ -27,6 +27,6 @@ export const analyzeBoard = (b: BoardInfo): AnalyzeResult => {
   if (rest3.length > 0) {
     return { rest: 3, expect: rest3 };
   }
-  return undefined;
+  return { rest: -1 };
 
 };
